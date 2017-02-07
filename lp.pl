807fbf06564917fd983677b83d2bdbdbf889c4bf
Ashok Tamang <ashoktamang52@gmail.com>
2017-02-07 14:16:34 -0500

    Implement part 2: 'sum-up-numbers-general'

--- lp.pl
+++ lp.pl
@@ -4,16 +4,34 @@
    	        the given list.
 */
 
-head-value(Element, 0):-
+head-value-simple(Element, 0):-
     \+ number(Element);
     is_list(Element).
-head-value(Element, Element) :- number(Element).
+head-value-simple(Element, Element) :- number(Element).
 
 sum-up-numbers-simple([],0).
 sum-up-numbers-simple([Head], 0) :-
 	\+ number(Head).
 sum-up-numbers-simple([Head | Tail], Total) :-
 	sum-up-numbers-simple(Tail, Sub_total),
-	head-value(Head, Head_total),
+	head-value-simple(Head, Head_total),
 	Total is Head_total + Sub_total.
 
+
+/* [Part 2]
+	Parameter: <list> <number>
+	Returns: True if <number> is the sum of all the numbers (including those in
+			nested lists) in <list>.
+*/
+
+head-value-general(Element, 0) :- \+ number(Element).
+head-value-general(Element, Element) :- number(Element).
+head-value-general(Element, Total):-
+	is_list(Element),
+	sum-up-numbers-simple(Element, Total).
+
+sum-up-numbers-general([], 0).
+sum-up-numbers-general([Head | Tail], Total) :-
+	sum-up-numbers-general(Tail, Sub_total),
+	head-value-general(Head, Head_total),
+	Total is Head_total + Sub_total.
